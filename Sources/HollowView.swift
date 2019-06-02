e8e1046f89298689d03caf554e22092ceb1e254c
Azhar <azharudheen@farabi.ae>
2019-06-02 17:17:58 +0400

    added camera border

--- Sources/HollowView.swift
+++ Sources/HollowView.swift
@@ -34,5 +34,16 @@ class HollowView: UIView {
         
         UIColor.clear.setFill();
         UIRectFill(holeRectIntersection);
+        
+        var aPath = UIBezierPath()
+        
+        aPath.move(to: CGPoint(x: holeRectIntersection.minX, y: holeRectIntersection.minY))
+        aPath.addLine(to: CGPoint(x: holeRectIntersection.maxX, y: holeRectIntersection.minY))
+        aPath.addLine(to: CGPoint(x: holeRectIntersection.maxX, y: holeRectIntersection.maxY))
+        aPath.addLine(to: CGPoint(x: holeRectIntersection.minX, y: holeRectIntersection.maxY))
+        aPath.close()
+        UIColor(red: 255/255, green: 107/255, blue: 0, alpha: 1.0).set()
+        aPath.lineWidth = 2.0
+        aPath.stroke()
     }
 }
